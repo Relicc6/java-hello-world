1ddd855d6ac4f24b493306b1458af434085601d3
Relicc6 <166353690+Relicc6@users.noreply.github.com>
2024-05-08 11:58:34 -0400

    Added my name and student ID

--- hello.java
+++ hello.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello CST8132!"); 
-        System.out.println("");
+        System.out.println("Eric Loiselle, 040957227");
     }
 }
